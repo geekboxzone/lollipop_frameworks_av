923b1e1a67534789f93063ec693548f2b9ae5aa3
Marco Nelissen <marcone@google.com>
2015-10-02 15:12:00 -0700

    Check NAL size before use
    Bug: 24441553
    Bug: 24445122
    Change-Id: Ib7f025769adbafd5a2cb64fae5562a0a565945c2
    (cherry picked from commit 5d101298d8b0a78a1dc5bd26dbdada411f4ecd4d)

--- media/libstagefright/MPEG4Extractor.cpp
+++ media/libstagefright/MPEG4Extractor.cpp
@@ -4421,7 +4421,10 @@ MPEG4_NEXT_SAMPLE:
             (const uint8_t *)mBuffer->data() + mBuffer->range_offset();
 
         size_t nal_size = parseNALSize(src);
-        if (mBuffer->range_length() < mNALLengthSize + nal_size) {
+        if (mNALLengthSize > SIZE_MAX - nal_size) {
+            ALOGE("b/24441553, b/24445122");
+        }
+        if (mBuffer->range_length() - mNALLengthSize < nal_size) {
             ALOGE("incomplete NAL unit.");
 
             mBuffer->release();
@@ -4754,7 +4757,11 @@ status_t MPEG4Source::fragmentedRead(
             (const uint8_t *)mBuffer->data() + mBuffer->range_offset();
 
         size_t nal_size = parseNALSize(src);
-        if (mBuffer->range_length() < mNALLengthSize + nal_size) {
+        if (mNALLengthSize > SIZE_MAX - nal_size) {
+            ALOGE("b/24441553, b/24445122");
+        }
+
+        if (mBuffer->range_length() - mNALLengthSize < nal_size) {
             ALOGE("incomplete NAL unit.");
 
             mBuffer->release();
